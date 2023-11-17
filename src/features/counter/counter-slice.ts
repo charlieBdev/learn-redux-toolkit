e75789bee87ddf472a16e163572c7bd4b5ae4215
charlieBdev <chazbish@hotmail.com>
2023-11-17 15:23:57 +0000

    fin create counter slice

--- src/features/counter/counter-slice.ts
+++ src/features/counter/counter-slice.ts
@@ -1,3 +1,32 @@
 // DUCKS pattern
 
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface CounterState {
+	value: number;
+}
+
+const initialState: CounterState = {
+	value: 0,
+};
+
+const counterSlice = createSlice({
+	name: 'counter',
+	initialState,
+	reducers: {
+		increment(state) {
+			// looks like it's mutating but it's not due to Redux magic (Immer)
+			state.value++;
+		},
+		decrement(state) {
+			state.value--;
+		},
+		reset(state) {
+			state.value = 0;
+		},
+	},
+});
+
+export const { increment, decrement, reset } = counterSlice.actions;
+
+export default counterSlice.reducer;
